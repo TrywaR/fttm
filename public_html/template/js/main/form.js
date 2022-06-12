2a89eb00889b0a2c08aa1b6970987fc7344dfb74
Сергей Ильин <TrywaR@yandex.ru>
2022-06-12 20:32:56 +0300

    swither fix

--- public_html/template/js/main/form.js
+++ public_html/template/js/main/form.js
@@ -2,7 +2,7 @@
 function forms_init( oForm ){
   // switch скрытые блоки
   $( oForm ).find('.switch select').each(function(){
-    oForm.find('.switch_' + $(this).attr('name') + '-' + $(this).val()).addClass('_slow_')
+    oForm.find('.switch_' + $(this).attr('name') + '-' + $(this).val()).addClass('_show_')
   })
 }
 
@@ -14,7 +14,7 @@ $(function(){
 
   // switch скрытые блоки
   $(document).on('change', '.switch select', function(){
-    $(this).parents('form').find('[class*=switch_' + $(this).attr('name') + ']').removeClass('_slow_')
-    $(this).parents('form').find('.switch_' + $(this).attr('name') + '-' + $(this).val()).addClass('_slow_')
+    $(this).parents('form').find('[class*=switch_' + $(this).attr('name') + ']').removeClass('_show_')
+    $(this).parents('form').find('.switch_' + $(this).attr('name') + '-' + $(this).val()).addClass('_show_')
   })
 })
